a5340d6f74de88ff5bf96fa0bd07c3fb5a1edcc5
SaschaSt <saschast@topdesk.com>
2022-03-25 08:36:56 +0100

    inital project setup

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#33A5E7',
+  scale: {
+    width: 800,
+    height: 600,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+};
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  backgroundColor: '#33A5E7',
+  scale: {
+    width: 800,
+    height: 600,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+};
+
+export { config };
